6fa2b4753038f4e159c477570e42940aadddf538
Carson Weeks <CarsonWeeks@mail.com>
2021-05-15 15:19:56 -0700

    Created model for Image Pin.

---
+++ README.md
@@ -0,0 +1,8 @@
+Muuvboard
+
+Objective
+Use libraries where needed first, than refactor out as needed.
+If something is too hard, quiet literally just skip it
+
+testuser123
+testpass123
--- server/boards/models.py
+++ server/boards/models.py
@@ -12,8 +12,24 @@ class Pin(models.Model):
         AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="pins"
     )
 
-    x_coordinate = models.IntegerField()
-    y_coordinate = models.IntegerField()
+    x_coordinate = models.IntegerField(default=0)
+    y_coordinate = models.IntegerField(default=0)
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+
+class ImagePin(Pin):
+    pass
+
+
+class Image(models.Model):
+    filename = models.CharField(max_length=255, blank=False)
+    mimetype = models.CharField(max_length=30, blank=False)
+
+    pin = models.ForeignKey(ImagePin, related_name="images", on_delete=models.DO_NOTHING)
+
+    image = models.ImageField(upload_to="images/")
+    # Thumbnails Later
+
+    created = models.DateTimeField(auto_now_add=True)
