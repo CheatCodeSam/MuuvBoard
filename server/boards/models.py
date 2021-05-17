3e3052c6c01eea06219930265d98d2a03cc6449a
Carson Weeks <CarsonWeeks@mail.com>
2021-05-17 13:25:37 -0700

    Board names must be unique.

--- server/boards/models.py
+++ server/boards/models.py
@@ -3,7 +3,7 @@ from taggit.managers import TaggableManager
 
 
 class Board(models.Model):
-    title = models.CharField(max_length=100, blank=False, null=True)
+    title = models.CharField(max_length=100, blank=False, null=False, unique=True)
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
