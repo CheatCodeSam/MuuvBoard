300e9290595b58074a8cb99050e14aa2efdb405e
Carson Weeks <CarsonWeeks@mail.com>
2021-05-16 20:51:18 -0700

    Update Board Title.

--- server/boards/views.py
+++ server/boards/views.py
@@ -1,3 +1,5 @@
+from os import stat
+
 from django.http import Http404
 from rest_framework import status
 from rest_framework.response import Response
@@ -33,3 +35,11 @@ class BoardDetail(APIView):
         board = self.get_object(pk)
         serializer = BoardSerializer(board)
         return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        board = self.get_object(pk)
+        serializer = BoardSerializer(board, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- server/test/boards/boards_tests.py
+++ server/test/boards/boards_tests.py
@@ -47,7 +47,7 @@ def test_create_board(client):
 
 @pytest.mark.django_db
 def test_get_single_board(client):
-    board = generate_board_with_pins("Fresh Board", 0)
+    board = generate_board_with_pins("Fresh Board")
     resp = client.get(f"/api/boards/{board.id}/")
     assert resp.status_code == 200
     assert resp.data["title"] == "Fresh Board"
@@ -56,3 +56,32 @@ def test_get_single_board(client):
 def test_get_incorrect_board(client):
     resp = client.get(f"/api/movies/-1/")
     assert resp.status_code == 404
+
+
+@pytest.mark.django_db
+def test_update_board_title(client):
+    board = generate_board_with_pins("Fresh Board")
+    resp = client.put(
+        f"/api/boards/{board.id}/",
+        {"title": "New Board Title"},
+        content_type="application/json",
+    )
+
+    assert resp.status_code == 200
+    assert resp.data["title"] == "New Board Title"
+
+    resp_two = client.get(f"/api/boards/{board.id}/")
+    assert resp_two.status_code == 200
+    assert resp_two.data["title"] == "New Board Title"
+
+
+@pytest.mark.django_db
+def test_update_board_invalid_json(client):
+    board = generate_board_with_pins("Fresh Board")
+    resp = client.put(
+        f"/api/boards/{board.id}/",
+        {""},
+        content_type="application/json",
+    )
+
+    assert resp.status_code == 400
