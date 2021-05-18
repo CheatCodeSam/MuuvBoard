d7f17507a6d95abd25ac9d41c875e14ff2db6cb8
Carson Weeks <CarsonWeeks@mail.com>
2021-05-17 17:31:13 -0700

    Removed seperate image table.

--- server/boards/models.py
+++ server/boards/models.py
@@ -11,24 +11,14 @@ class Board(models.Model):
 
 class Pin(models.Model):
     title = models.CharField(max_length=100, blank=True, null=True)
-    tags = TaggableManager()
+    tags = TaggableManager(blank=True)
 
     board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name="pins")
+
+    image = models.ImageField(upload_to="images/", null=True)
 
     x_coordinate = models.IntegerField(default=0)
     y_coordinate = models.IntegerField(default=0)
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-
-
-class Image(models.Model):
-    filename = models.CharField(max_length=255, blank=False)
-    mimetype = models.CharField(max_length=30, blank=False)
-
-    pin = models.ForeignKey(Pin, related_name="images", on_delete=models.DO_NOTHING)
-
-    image = models.ImageField(upload_to="images/")
-    # Thumbnails Later
-
-    created = models.DateTimeField(auto_now_add=True)
