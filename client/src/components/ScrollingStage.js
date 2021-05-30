9ae42f71d17a63a4972681224d91a90e6123c1c5
Carson Weeks <CarsonWeeks@mail.com>
2021-05-29 22:24:23 -0700

    Tool to track pointer position.

--- client/src/components/ScrollingStage.js
+++ client/src/components/ScrollingStage.js
@@ -1,20 +1,16 @@
 import React from 'react'
-import { Stage, Layer, Rect } from 'react-konva';
+import { Stage, Layer, Rect, Group, Text } from 'react-konva';
 
 
 const stageStyles = {
-    backgroundImage: "radial-gradient(#444cf7 0.5px, #e5e5f7 0.5px)",
-    backgroundSize: "10px 10px",
     backgroundColor: "#e5e5f7",
-    opacity: "0.8"
 };
 
 class ScrollingStage extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { width: 0, height: 0 };
-        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
-
+        this.state = { width: 0, height: 0, mousePos: { x: 0, y: 0 } };
+        this.layerRef = React.createRef();
     }
 
     componentDidMount() {
@@ -26,14 +22,31 @@ class ScrollingStage extends React.Component {
         window.removeEventListener('resize', this.updateWindowDimensions);
     }
 
-    updateWindowDimensions() {
+    updateWindowDimensions = () => {
         this.setState({ width: window.innerWidth, height: window.innerHeight });
     }
 
     render() {
         return (
-            <Stage width={this.state.width} height={this.state.height} style={stageStyles}  >
-                <Layer offsetX={-window.innerWidth / 2} offsetY={-window.innerHeight / 2}>
+            <Stage width={this.state.width} height={this.state.height} style={stageStyles} onMouseMove={e => {
+                var transform = this.layerRef.current.getAbsoluteTransform().copy();
+                transform.invert();
+                const pos = e.target.getStage().getPointerPosition();
+                var circlePos = transform.point(pos);
+                this.setState({ mousePos: circlePos });
+            }}
+            >
+                <Layer>
+                    <Group x={0} y={0}>
+                        <Rect
+                            width={100}
+                            height={20}
+                            fill="black"
+                            opacity={0.3} />
+                        <Text text={`${this.state.mousePos.x.toFixed()}, ${this.state.mousePos.y.toFixed()}`} fill="white" align="center" verticalAlign="middle" width={100} height={20} fontSize={15} />
+                    </Group>
+                </Layer>
+                <Layer offsetX={-window.innerWidth / 2} offsetY={-window.innerHeight / 2} ref={this.layerRef}>
                     <Rect
                         x={0}
                         y={0}
