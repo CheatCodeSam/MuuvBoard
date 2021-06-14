8792b3aaa09f69d81d9cec8c90b9dbd9330be060
Carson Weeks <CarsonWeeks@mail.com>
2021-06-13 17:08:37 -0700

    Added Home Button.

--- client/src/components/Toolbar.js
+++ client/src/components/Toolbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import { Link } from "react-router-dom";
+import { ChevronDownIcon, HomeIcon } from '@heroicons/react/outline'
 
 
 class Toolbar extends React.Component {
@@ -7,10 +8,19 @@ class Toolbar extends React.Component {
     render() {
         return (
             <div className='toolbar'>
+                <Link to="/">
+                    <div className="home-container">
+                        <HomeIcon style={{ height: "20px", width: "20px", color: "white" }} />
+                    </div>
+                </Link>
+
                 <div className="filename-view">
                     <span className='filename-title'>
                         {this.props.title}
                     </span>
+                    <div className="chevron-container">
+                        <ChevronDownIcon style={{ height: "10px", width: "10px" }} />
+                    </div>
                 </div>
             </div>
         )
