aedd568ed9d7310873d1a83e40b610e9da8e35b3
Carson Weeks <CarsonWeeks@mail.com>
2021-06-15 17:02:42 -0700

    Added form to PinEditor.

--- client/src/components/PinEditor.js
+++ client/src/components/PinEditor.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Formik } from 'formik';
+
 
 class PinEditor extends React.Component {
 
@@ -10,7 +12,39 @@ class PinEditor extends React.Component {
     render() {
         return (
             <div className="pin-editor" onClick={this.props.onEscape}>
+                <div className="pin-editor-form" onClick={e => e.stopPropagation()} >
 
+
+                    <Formik
+                        initialValues={{ image: null }}
+                    >
+                        {({ values, handleSubmit, setFieldValue, handleChange }) => (
+                            <form onSubmit={handleSubmit}>
+                                <div className='form-group'>
+                                    <label htmlFor="title">Title</label>
+                                    <input
+                                        id="title"
+                                        name="title"
+                                        type="text"
+                                        onChange={handleChange}
+                                        value={values.title}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="file">File upload</label>
+                                    <input id="file" name="file" type="file" onChange={(event) => {
+                                        setFieldValue("file", event.currentTarget.files[0]);
+                                    }} className="form-control" />
+                                </div>
+                                <button type="submit" >submit</button>
+                            </form>
+                        )}
+
+                    </Formik>
+
+
+
+                </div>
             </div>
         )
     }
