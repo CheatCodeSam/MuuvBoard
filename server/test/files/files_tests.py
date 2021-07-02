aa1e39559e3a4a49bd97a6a39bd6cd4a3f8b9e4f
Carson Weeks <CarsonWeeks@mail.com>
2021-07-02 12:13:04 -0700

    Added test for file upload.

--- server/test/files/files_tests.py
+++ server/test/files/files_tests.py
@@ -7,14 +7,16 @@ from boards.models import Board
 from files.models import Image
 from pins.serializers import PinSerializer
 
+# TODO Test for conversions
+
 
 @pytest.mark.django_db
-def test_create_pin(client, generate_board_with_pins, generate_image):
+def test_create_file(client, generate_image):
 
     tmp_file = generate_image("test.png")
 
     resp = client.post(
-        f"/api/pins/",
+        f"/api/files/",
         {"image": tmp_file},
         format="multipart",
     )
@@ -22,6 +24,3 @@ def test_create_pin(client, generate_board_with_pins, generate_image):
     assert resp.status_code == 201
     id = resp.data["id"]
     assert Image.objects.get(pk=id)
-
-
-# TODO Test for conversions
