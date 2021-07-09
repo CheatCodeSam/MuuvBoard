ddc85ac268aad3ae3652bf5eecb1df95a71dbe9c
Carson Weeks <CarsonWeeks@mail.com>
2021-07-09 14:45:41 -0700

    Added token auth as default auth.

--- server/muuvboard/settings.py
+++ server/muuvboard/settings.py
@@ -141,8 +141,12 @@ CORS_ORIGIN_WHITELIST = ["http://localhost:3001", "http://localhost"]
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
-# REST_FRAMEWORK = {
-#     "DEFAULT_PERMISSION_CLASSES": [
-#         "rest_framework.permissions.IsAuthenticated",
-#     ]
-# }
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+}
