388b18455e60bc0ecc9f8af2696b9ed754b230d0
Carson Weeks <CarsonWeeks@mail.com>
2021-07-09 16:10:49 -0700

    Forces user to login screen if no Token.

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,28 @@ import Board from './components/Board';
 
 import ListOfBoards from './components/ListOfBoards'
 
+import LogIn from './components/LogIn';
 
 
+const setToken = (userToken) => {
+    sessionStorage.setItem('token', JSON.stringify(userToken))
+}
+
+const getToken = () => {
+
+}
 
 function App() {
+    const token = getToken()
+
+    if (!token) {
+        return <LogIn setToken={setToken} />
+    }
 
     return (
         <>
             <Switch>
+                <Route path="/board/:BoardId/" component={Board} />
                 <Route path="/board/:BoardId/" component={Board} />
                 <Route path="/" component={ListOfBoards} />
             </Switch>
