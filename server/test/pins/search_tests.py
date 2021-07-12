0fe844d2b307d9c617daf1519d3f468e437ed604
Carson Weeks <CarsonWeeks@mail.com>
2021-07-11 17:21:03 -0700

    Added test for duplicate search results.

--- server/test/pins/search_tests.py
+++ server/test/pins/search_tests.py
@@ -45,3 +45,27 @@ def test_can_only_search_pins_owned_by_user(
     assert resp.status_code == 200
 
     assert not resp.data
+
+
+@pytest.mark.django_db
+def test_no_duplicates(client, generate_board_with_pins, create_user):
+    user = create_user()
+    board = generate_board_with_pins("Fresh Board", user, 2)
+    firstpin = board.pins.first()
+    firstpin.title = "this is my title"
+    firstpin.tags.add("title", "hello")
+    firstpin.save()
+
+    second_pin = board.pins.last()
+    second_pin.title = "this is my pin"
+    second_pin.tags.add("title", "hello")
+    second_pin.save()
+
+    client.force_login(user)
+    resp = client.get(f"/api/pins/?search=title&board={board.id}")
+
+    assert resp.status_code == 200
+    assert len(resp.data) == 2
+    assert resp.data[0]["id"] != resp.data[1]["id"]
+    assert resp.data[0]["id"] == firstpin.id or resp.data[0]["id"] == second_pin.id
+    assert resp.data[1]["id"] == firstpin.id or resp.data[1]["id"] == second_pin.id
