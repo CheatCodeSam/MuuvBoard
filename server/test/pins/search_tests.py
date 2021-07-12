3d8fa7b4d80c495481dcbe82b512e126b24bfef5
Carson Weeks <CarsonWeeks@mail.com>
2021-07-11 17:28:49 -0700

    Added test for searching tags.

--- server/test/pins/search_tests.py
+++ server/test/pins/search_tests.py
@@ -1,22 +1,29 @@
-import json
 from test.conftest import create_user, generate_board_with_pins
 
 import pytest
-from django.core.files.images import ImageFile
-
-from boards.models import Board
-from files.models import Image
-from pins.models import Pin
-from pins.serializers import PinSerializer
 
 
-# TODO add more search test
 @pytest.mark.django_db
 def test_search_pin_titles(client, generate_board_with_pins, create_user):
     user = create_user()
     board = generate_board_with_pins("Fresh Board", user, 2)
     firstpin = board.pins.first()
     firstpin.title = "this is my title"
+    firstpin.save()
+    client.force_login(user)
+    resp = client.get(f"/api/pins/?search=title&board={board.id}")
+
+    assert resp.status_code == 200
+
+    assert resp.data[0]["id"] == firstpin.id
+
+
+@pytest.mark.django_db
+def test_search_pin_tags(client, generate_board_with_pins, create_user):
+    user = create_user()
+    board = generate_board_with_pins("Fresh Board", user, 2)
+    firstpin = board.pins.first()
+    firstpin.tags.add("title", "tag", "hello", "world")
     firstpin.save()
     client.force_login(user)
     resp = client.get(f"/api/pins/?search=title&board={board.id}")
