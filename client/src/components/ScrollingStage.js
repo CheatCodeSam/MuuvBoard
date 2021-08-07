64c76fb85dc09cc724a39802fd6fd2db5f418bfb
Carson Weeks <CarsonWeeks@mail.com>
2021-08-06 22:16:34 -0700

    Sped up project.

--- client/src/components/ScrollingStage.js
+++ client/src/components/ScrollingStage.js
@@ -1,33 +1,32 @@
-import React from 'react'
-import { Stage, Layer, Rect } from 'react-konva';
-import Konva from "konva";
-import ContextMenu from './ContextMenu';
-import ImagePin from './ImagePin'
-import SelectionBoxEvents from './SelectionBoxEvents';
-import ContextMenuEvents from './ContextMenuEvents';
+import React from "react"
+import { Stage, Layer, Rect } from "react-konva"
+import Konva from "konva"
+import ContextMenu from "./ContextMenu"
+import ImagePin from "./ImagePin"
+import SelectionBoxEvents from "./SelectionBoxEvents"
+import ContextMenuEvents from "./ContextMenuEvents"
 
+const MOUSEONE = 0
+const MOUSETWO = 2
+const MOUSETHREE = 1
 
-const MOUSEONE = 0;
-const MOUSETWO = 2;
-const MOUSETHREE = 1;
+const SCROLLINGSPEED = 17
 
-const SCROLLINGSPEED = 17;
-
+Konva.pixelRatio = 1
 
 //! REACT WAS NOT MADE TO SUPPORT CANVAS
 // All rules involving how React should operate
-// should be taken with a grain of salt. We are 
-// not working with a DOM, we are working 
+// should be taken with a grain of salt. We are
+// not working with a DOM, we are working
 // with a canvas.
 class ScrollingStage extends React.Component {
-
     constructor(props) {
-        super(props);
-        this.setState = this.setState.bind(this);
+        super(props)
+        this.setState = this.setState.bind(this)
         this.stage = React.createRef()
 
-        this.selectionBox = new SelectionBoxEvents(this.setState);
-        this.contextMenu = new ContextMenuEvents(this.setState);
+        this.selectionBox = new SelectionBoxEvents(this.setState)
+        this.contextMenu = new ContextMenuEvents(this.setState)
         this.state = {
             grab: false,
             dragging: false,
@@ -42,15 +41,12 @@ class ScrollingStage extends React.Component {
         }
     }
 
-
     move = (x, y) => {
-
-
         this.props.onStagePan({ x: x, y: y })
     }
 
     // TODO move all this into seperate class.
-    scrollStageIfNecessary = (mousePos) => {
+    scrollStageIfNecessary = mousePos => {
         if (mousePos.x < 15) {
             this.moveLeft()
         } else if (mousePos.x > this.props.width - 15) {
@@ -74,8 +70,8 @@ class ScrollingStage extends React.Component {
                 this.move(this.props.x - SCROLLINGSPEED, this.props.y)
                 const movement = {
                     x: SCROLLINGSPEED,
-                    y: 0
-                };
+                    y: 0,
+                }
                 this.moveSelectedPins(movement)
             }, 1000 / 60)
             this.setState({ horizontalPanningInterval: intervalId })
@@ -88,12 +84,11 @@ class ScrollingStage extends React.Component {
                 this.move(this.props.x + SCROLLINGSPEED, this.props.y)
                 const movement = {
                     x: -SCROLLINGSPEED,
-                    y: 0
-                };
+                    y: 0,
+                }
                 this.moveSelectedPins(movement)
             }, 1000 / 60)
             this.setState({ horizontalPanningInterval: intervalId })
-
         }
     }
     stopHorizontalPanning = () => {
@@ -109,8 +104,8 @@ class ScrollingStage extends React.Component {
                 this.move(this.props.x, this.props.y + SCROLLINGSPEED)
                 const movement = {
                     x: 0,
-                    y: -SCROLLINGSPEED
-                };
+                    y: -SCROLLINGSPEED,
+                }
                 this.moveSelectedPins(movement)
             }, 1000 / 60)
             this.setState({ verticalPanningInterval: intervalId })
@@ -123,8 +118,8 @@ class ScrollingStage extends React.Component {
                 this.move(this.props.x, this.props.y - SCROLLINGSPEED)
                 const movement = {
                     x: 0,
-                    y: SCROLLINGSPEED
-                };
+                    y: SCROLLINGSPEED,
+                }
                 this.moveSelectedPins(movement)
             }, 1000 / 60)
             this.setState({ verticalPanningInterval: intervalId })
@@ -141,37 +136,34 @@ class ScrollingStage extends React.Component {
         this.stopVerticalPanning()
     }
 
-
-
     // ===== LIFE CYCLE =====
 
-    componentDidMount() {
-        window.addEventListener('mouseup', this.onMouseUpWindow)
-        window.addEventListener('mousemove', this.onMouseMoveWindow)
-    }
+    componentDidMount() {}
 
-    componentWillUnmount() {
-        window.removeEventListener('mouseup', this.onMouseUpWindow);
-        window.removeEventListener('mousemove', this.onMouseMoveWindow)
-    }
+    componentWillUnmount() {}
 
     // ===== UTIL =====
 
-    calculateStageOffset = (coords) => {
-        return { x: coords.x - this.props.x, y: coords.y - this.props.y };
+    calculateStageOffset = coords => {
+        return { x: coords.x - this.props.x, y: coords.y - this.props.y }
     }
 
-    getSelectedPins = () => this.props.pins.filter(pin => pin.selected);
+    getSelectedPins = () => this.props.pins.filter(pin => pin.selected)
 
-    getSelectedPinsIds = () => this.props.pins.filter(pin => pin.selected).map(pin => pin.id)
+    getSelectedPinsIds = () =>
+        this.props.pins.filter(pin => pin.selected).map(pin => pin.id)
 
-    getPinById = (id) => this.props.pins.find(pin => pin.id === id);
+    getPinById = id => this.props.pins.find(pin => pin.id === id)
 
-    getCenterPointOfPins = (ids) => {
-
+    getCenterPointOfPins = ids => {
         const pinCoords = ids.map(id => {
             const pin = this.getPinById(id)
-            return { x: pin.x_coordinate, y: pin.y_coordinate, width: 135, height: 160 }
+            return {
+                x: pin.x_coordinate,
+                y: pin.y_coordinate,
+                width: 135,
+                height: 160,
+            }
         })
         if (pinCoords.length == 0) {
             return null
@@ -186,34 +178,35 @@ class ScrollingStage extends React.Component {
         const midPointY = (y1 + y2) / 2
 
         return { x: midPointX, y: midPointY }
-
     }
-
 
     // ===== PROPS REQUEST =====
 
-    selectPins = (ids) => this.props.onPinSelect(ids)
+    selectPins = ids => this.props.onPinSelect(ids)
 
-    moveSelectedPins = (coords) => this.props.onPinMove(coords, this.getSelectedPinsIds())
+    moveSelectedPins = coords =>
+        this.props.onPinMove(coords, this.getSelectedPinsIds())
 
     deleteSelectedPins = () => this.props.onPinDelete(this.getSelectedPinsIds())
 
-    onPinCreate = (pin) => this.props.onPinCreate(pin)
+    onPinCreate = pin => this.props.onPinCreate(pin)
 
     onPinMoveEnd = () => this.props.onPinMoveEnd(this.getSelectedPins())
 
     // ===== STAGE/WINDOW EVENTS =====
 
-    onMouseDownOnStage = (e) => {
-        const stage = this.stage.current;
-        const { target } = e;
+    onMouseDownOnStage = e => {
+        const stage = this.stage.current
+        const { target } = e
 
         this.contextMenu.hideContextMenu()
 
         if (e.evt.button === MOUSEONE) {
             if (target === stage) {
                 this.selectPins([])
-                const coords = this.calculateStageOffset(stage.getPointerPosition())
+                const coords = this.calculateStageOffset(
+                    stage.getPointerPosition()
+                )
                 this.selectionBox.selectionBoxCreate(coords)
             } else if (target.parent.name() === "pin") {
                 const pin = this.getPinById(target.parent.id())
@@ -224,44 +217,59 @@ class ScrollingStage extends React.Component {
         }
 
         if (e.evt.button === MOUSETHREE) {
-            e.evt.preventDefault();
+            e.evt.preventDefault()
             this.selectionBox.hideSelectionBox()
-            this.setState({ grab: true });
+            this.setState({ grab: true })
         }
+
+        window.addEventListener("mouseup", this.onMouseUpWindow)
+        window.addEventListener("mousemove", this.onMouseMoveWindow)
     }
 
-    onMouseUpWindow = (e) => {
-        const stage = this.stage.current;
+    onMouseUpWindow = e => {
+        const stage = this.stage.current
         this.stopPanning()
 
         if (this.selectionBox.isVisible(this.state)) {
-            const pinShapes = stage.find('.pin')
-            const selectedPins = this.selectionBox.selectionBoxEnd(this.state, pinShapes)
-            this.selectPins(selectedPins.map((a) => a.id()))
+            const pinShapes = stage.find(".pin")
+            const selectedPins = this.selectionBox.selectionBoxEnd(
+                this.state,
+                pinShapes
+            )
+            this.selectPins(selectedPins.map(a => a.id()))
         }
 
         if (this.state.grab) {
-            this.setState({ grab: false });
+            this.setState({ grab: false })
         }
+
+        window.removeEventListener("mouseup", this.onMouseUpWindow)
+        window.removeEventListener("mousemove", this.onMouseMoveWindow)
     }
 
-    onMouseMoveWindow = (e) => {
-        const stage = this.stage.current;
+    onMouseMoveWindow = e => {
+        const stage = this.stage.current
         if (this.selectionBox.isVisible(this.state)) {
             // * Because were using mouseMoveWindow we have to account for the 48 px height of the toolbar
             const stagePosition = stage.content.getBoundingClientRect()
 
-            let newY = Math.max((e.clientY - stagePosition.y), 0)
-            newY = (e.clientY - stagePosition.y) > this.props.height ? this.props.height : newY;
-            let newX = Math.max((e.clientX - stagePosition.x), 0)
-            newX = (e.clientX - stagePosition.x) > this.props.width ? this.props.width : newX;
+            let newY = Math.max(e.clientY - stagePosition.y, 0)
+            newY =
+                e.clientY - stagePosition.y > this.props.height
+                    ? this.props.height
+                    : newY
+            let newX = Math.max(e.clientX - stagePosition.x, 0)
+            newX =
+                e.clientX - stagePosition.x > this.props.width
+                    ? this.props.width
+                    : newX
 
             const coords = this.calculateStageOffset({ x: newX, y: newY })
             this.selectionBox.selectionBoxMove(coords)
         }
     }
 
-    onStageDrag = (e) => {
+    onStageDrag = e => {
         if (this.state.grab) {
             const { target } = e
             this.move(target.x(), target.y())
@@ -270,52 +278,54 @@ class ScrollingStage extends React.Component {
 
     // ===== PIN EVENTS =====
 
-    onPinDragStart = (e) => {
-        const { target } = e;
-        const pin = this.getPinById(target.id());
+    onPinDragStart = e => {
+        const { target } = e
+        const pin = this.getPinById(target.id())
 
         if (!!!pin.selected) {
             this.selectPins([pin.id])
         }
     }
 
-    onPinDrag = (e) => {
-        const { target } = e;
-        const stage = this.stage.current;
+    onPinDrag = e => {
+        const { target } = e
+        const stage = this.stage.current
 
         const pin = this.getPinById(target.id())
         const movement = {
             x: target.x() - pin.x_coordinate,
-            y: target.y() - pin.y_coordinate
-        };
+            y: target.y() - pin.y_coordinate,
+        }
 
         const stagePosition = stage.content.getBoundingClientRect()
-        this.scrollStageIfNecessary({ x: window.event.clientX - stagePosition.x, y: window.event.clientY - stagePosition.y })
+        this.scrollStageIfNecessary({
+            x: window.event.clientX - stagePosition.x,
+            y: window.event.clientY - stagePosition.y,
+        })
 
         this.moveSelectedPins(movement)
-
     }
 
-    onPinDragEnd = (e) => this.onPinMoveEnd()
+    onPinDragEnd = e => this.onPinMoveEnd()
 
-    onPinDoubleClick = (id) => this.props.onPinView(id)
+    onPinDoubleClick = id => this.props.onPinView(id)
 
-    dragBoundFunc = (pos) => {
+    dragBoundFunc = pos => {
         let newY = Math.max(pos.y, 0 - 160)
-        newY = pos.y > this.props.height ? this.props.height : newY;
+        newY = pos.y > this.props.height ? this.props.height : newY
         let newX = Math.max(pos.x, 0 - 135)
-        newX = pos.x > this.props.width ? this.props.width : newX;
+        newX = pos.x > this.props.width ? this.props.width : newX
         return {
             x: newX,
             y: newY,
-        };
+        }
     }
 
     // ===== CONTEXT MENU =====
 
-    onContextMenu = (e) => {
-        const stage = this.stage.current;
-        const { target } = e;
+    onContextMenu = e => {
+        const stage = this.stage.current
+        const { target } = e
 
         e.evt.preventDefault()
         this.selectionBox.hideSelectionBox()
@@ -334,60 +344,69 @@ class ScrollingStage extends React.Component {
     generateContextMenuOptions = () => {
         const { x, y } = this.contextMenu.getCoords(this.state)
         let returnValue = []
-        returnValue.push({ name: 'Create Pin', func: () => { this.props.showPinEditor(x, y); this.contextMenu.hideContextMenu(); } })
+        returnValue.push({
+            name: "Create Pin",
+            func: () => {
+                this.props.showPinEditor(x, y)
+                this.contextMenu.hideContextMenu()
+            },
+        })
         if (!!this.getSelectedPins().length) {
-            returnValue.push({ name: "Delete Pin", func: () => { this.deleteSelectedPins(); this.contextMenu.hideContextMenu(); } })
+            returnValue.push({
+                name: "Delete Pin",
+                func: () => {
+                    this.deleteSelectedPins()
+                    this.contextMenu.hideContextMenu()
+                },
+            })
         }
         return returnValue
     }
 
     // ===== SCROLL =====
 
-    onWheel = (e) => {
+    onWheel = e => {
         e.evt.preventDefault()
         this.selectionBox.hideSelectionBox()
         this.contextMenu.hideContextMenu()
         this.move(this.props.x - e.evt.deltaX, this.props.y - e.evt.deltaY)
     }
 
-
     render() {
         const offsetStyle = {
-            backgroundPosition:
-                this.props.x + "px " + this.props.y + "px"
+            // backgroundPosition:
+            //     this.props.x + "px " + this.props.y + "px"
         }
         const selc = this.selectionBox.calculateSelectionBox(this.state)
         const conMenu = this.contextMenu.getCoords(this.state)
 
-        const midPointOfSelection = this.getCenterPointOfPins(this.getSelectedPinsIds())
+        const midPointOfSelection = this.getCenterPointOfPins(
+            this.getSelectedPinsIds()
+        )
 
         return (
             <div
-                tabIndex='0'
+                tabIndex="0"
                 className={"stage-view " + (this.state.grab ? "grabbing" : "")}
             >
-
                 <Stage
                     height={this.props.height}
                     width={this.props.width}
                     style={offsetStyle}
-                    className='scrolling-stage'
+                    className="scrolling-stage"
                     ref={this.stage}
                     onContextMenu={this.onContextMenu}
                     onMouseDown={this.onMouseDownOnStage}
                     draggable={this.state.grab}
                     onDragMove={this.onStageDrag}
-
                     onWheel={this.onWheel}
-
                     x={this.props.x}
                     y={this.props.y}
                 >
-
                     <Layer>
-                        {
-                            this.props.pins.map((pin) => {
-                                return <ImagePin
+                        {this.props.pins.map(pin => {
+                            return (
+                                <ImagePin
                                     key={pin.id}
                                     id={pin.id}
                                     name="pin"
@@ -403,8 +422,8 @@ class ScrollingStage extends React.Component {
                                     onDblClick={this.onPinDoubleClick}
                                     dragBoundFunc={this.dragBoundFunc}
                                 />
-                            })
-                        }
+                            )
+                        })}
 
                         {midPointOfSelection && (
                             <Rect
@@ -431,7 +450,7 @@ class ScrollingStage extends React.Component {
                         {this.state.rightSideOfScreen && (
                             <Rect
                                 name="e"
-                                x={(this.props.width - 15) - this.props.x}
+                                x={this.props.width - 15 - this.props.x}
                                 y={0 - this.props.y}
                                 height={this.props.height}
                                 width={15}
@@ -454,14 +473,13 @@ class ScrollingStage extends React.Component {
                             <Rect
                                 name="e"
                                 x={0 - this.props.x}
-                                y={(this.props.height - 15) - this.props.y}
+                                y={this.props.height - 15 - this.props.y}
                                 height={15}
                                 width={this.props.width}
                                 fill="red"
                                 opacity={0.5}
                             />
                         )}
-
 
                         {this.contextMenu.isVisible(this.state) && (
                             <ContextMenu
@@ -471,11 +489,7 @@ class ScrollingStage extends React.Component {
                             />
                         )}
                     </Layer>
-                    <Layer
-                        name="selectionBoxLayer"
-                        listening={false}
-
-                    >
+                    <Layer name="selectionBoxLayer" listening={false}>
                         {this.selectionBox.isVisible(this.state) && (
                             <Rect
                                 name="selection"
@@ -489,16 +503,11 @@ class ScrollingStage extends React.Component {
                                 stroke="blue"
                             />
                         )}
-
                     </Layer>
                 </Stage>
-
             </div>
         )
-
     }
-
 }
 
-
-export default ScrollingStage;
+export default ScrollingStage
